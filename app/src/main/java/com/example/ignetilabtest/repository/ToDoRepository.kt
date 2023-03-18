ccd8d39802666233a7adf0ce8e12934f66c928fa
Waseem Chandio <waseemchandio2k16@gmail.com>
2023-03-19 00:36:43 +0500

    setup repository

---
+++ app/src/main/java/com/example/ignetilabtest/repository/ToDoRepository.kt
@@ -0,0 +1,47 @@
+package com.example.ignetilabtest.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.example.ignetilabtest.database.AppDatabase
+import com.example.ignetilabtest.database.TodoRecord
+import com.example.ignetilabtest.database.UserDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class ToDoRepository (application: Application) {
+
+    private val todoDao: UserDao
+    private val allTodos: LiveData<List<TodoRecord>>
+
+    init {
+        val database = AppDatabase.getInstance(application.applicationContext)
+        todoDao = database!!.userDao()
+        allTodos = todoDao.getAllTodoList()
+    }
+
+    fun saveTodo(todo: TodoRecord) = runBlocking {
+        this.launch(Dispatchers.IO) {
+            todoDao.saveTodo(todo)
+        }
+    }
+
+    fun updateTodo(todo: TodoRecord) = runBlocking {
+        this.launch(Dispatchers.IO) {
+            todoDao.updateTodo(todo)
+        }
+    }
+
+
+    fun deleteTodo(todo: TodoRecord) {
+        runBlocking {
+            this.launch(Dispatchers.IO) {
+                todoDao.deleteTodo(todo)
+            }
+        }
+    }
+
+    fun getAllTodoList(): LiveData<List<TodoRecord>> {
+        return allTodos
+    }
+}
