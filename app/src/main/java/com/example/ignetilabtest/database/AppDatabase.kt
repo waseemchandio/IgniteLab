58d58072d0d28c5f98ab4372af75ffb9cb85cd90
Waseem Chandio <waseemchandio2k16@gmail.com>
2023-03-19 01:25:38 +0500

    set todo validation and set ui

--- app/src/main/java/com/example/ignetilabtest/database/AppDatabase.kt
+++ app/src/main/java/com/example/ignetilabtest/database/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [TodoRecord::class], version = 1)
+@Database(entities = [TodoRecord::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 
--- app/src/main/java/com/example/ignetilabtest/fragments/homeFragment/HomeFragment.kt
+++ app/src/main/java/com/example/ignetilabtest/fragments/homeFragment/HomeFragment.kt
@@ -1,22 +1,54 @@
 package com.example.ignetilabtest.fragments.homeFragment
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.ignetilabtest.R
+import com.example.ignetilabtest.database.TodoRecord
+import com.example.ignetilabtest.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
+    var todoRecord: TodoRecord? = null
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+
+        binding.btnSave.setOnClickListener {
+            saveTodo()
+        }
+
+        val view = binding.root
+        return view
+    }
+
+
+
+    private fun saveTodo() {
+        if (validateFields()) {
+
+
+        }
+    }
+
+    private fun validateFields(): Boolean {
+        if (binding.etTodoTitle.text.isEmpty()) {
+            binding.tilTodoTitle.error = getString(R.string.pleaseEnterTitle)
+            binding.etTodoTitle.requestFocus()
+            return false
+        }
+        if (binding.etTodoContent.text.isEmpty()) {
+            binding.tilTodoContent.error = getString(R.string.pleaseEnterContent)
+            binding.etTodoContent.requestFocus()
+            return false
+        }
+        return true
     }
 
 }
