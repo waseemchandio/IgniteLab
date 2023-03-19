f6fa926ffced317e70bc9a2442f3e2c63b5596cb
Waseem Chandio <waseemchandio2k16@gmail.com>
2023-03-19 10:40:13 +0500

    delet todo item

--- app/src/main/java/com/example/ignetilabtest/adaptors/TodoListAdapter.kt
+++ app/src/main/java/com/example/ignetilabtest/adaptors/TodoListAdapter.kt
@@ -3,11 +3,13 @@ package com.example.ignetilabtest.adaptors
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ignetilabtest.database.TodoRecord
 import com.example.ignetilabtest.databinding.TodoItemBinding
+import com.example.ignetilabtest.models.TodoViewModel
 
-class TodoListAdapter (var context: Context, val list: List<TodoRecord>) :
+class TodoListAdapter(var context: Context, val list: List<TodoRecord>) :
     RecyclerView.Adapter<TodoListAdapter.TodoViewHolder>() {
 
     // Holds the TextView that will add each item to
@@ -23,12 +25,18 @@ class TodoListAdapter (var context: Context, val list: List<TodoRecord>) :
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        holder.binding.tvItemTitle.setText(""+list[position].title)
-        holder.binding.tvItemContent.setText(""+list[position].content)
+        holder.binding.tvItemTitle.setText("" + list[position].title)
+        holder.binding.tvItemContent.setText("" + list[position].content)
 
 
         holder.binding.ivItemDelete.setOnClickListener {
-
+            val todoViewModel = TodoViewModel(context)
+            val todo = TodoRecord(
+                id = list[position].id,
+                title = list[position].title,
+                content = list[position].content
+            )
+            todoViewModel.deleteTodo(todo)
 
         }
     }
